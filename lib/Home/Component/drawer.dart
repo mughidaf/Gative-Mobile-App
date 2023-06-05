769ba6a85bf8b1f30597f7ff5fea9baed6530e32
fahrigunawan <fahrigilo991@gmail.com>
2023-06-05 09:53:09 +0700

    Sidebar Done

--- lib/Home/Component/drawer.dart
+++ lib/Home/Component/drawer.dart
@@ -8,38 +8,30 @@ class Sidebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(20),
-            color: Color(0xFF1A1A1A),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    margin: EdgeInsets.only(top: 30,),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(image: AssetImage("assets/slide/ryujin2.jpg"), fit: BoxFit.fill),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10,),
-                    child: Text("Pari", style: TextStyle(fontSize: 20, color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 0),
-                    child: Text("Ur gae", style: TextStyle(fontSize: 16, color: Colors.white),),
-                  )
-                ],
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Pari gf'),
+            accountEmail: Text('pari@gmail.com'),
+            currentAccountPicture: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/slide/ryujin1.jpg"),fit: BoxFit.cover),
+                shape: BoxShape.circle,
               ),
             ),
-          ),     
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                      'assets/slide/bacl.png')
+                      ),
+            ),
+          ),
+          
           ListTile(
-            leading: Icon(Icons.shopping_bag, color: Color(0xFFFFAC42),),
+            leading: Icon(Icons.shopping_bag),
             title: Text("Shopping Cart"),
             onTap: () {
               Navigator.push(
@@ -49,7 +41,7 @@ class Sidebar extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(Icons.favorite_rounded, color: Color(0xFFFFAC42),),
+            leading: Icon(Icons.favorite_rounded),
             title: Text("WishList"),
             onTap: () {
               Navigator.push(
@@ -60,11 +52,11 @@ class Sidebar extends StatelessWidget {
           ),
           Divider(thickness: 1,),
           ListTile(
-            leading: Icon(Icons.settings_accessibility, color: Color(0xFFFFAC42),),
+            leading: Icon(Icons.account_box),
             title: Text("Settings"),
           ),
           ListTile(
-            leading: Icon(Icons.logout_sharp, color: Color(0xFFFFAC42),),
+            leading: Icon(Icons.logout),
             title: Text("Logout"),
           )
         ],
