3666428e07015bf8d45711a7705d0fac4020148e
SyamDN <76995702+SyamDN@users.noreply.github.com>
2023-06-16 00:00:27 +0700

    Penambahan page profile

--- lib/Home/pages/profile.dart
+++ lib/Home/pages/profile.dart
@@ -13,7 +13,9 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF333333),
       appBar: AppBar(
+        backgroundColor: Color(0xFF1A1A1A),
         title: Text('Profile'),
       ),
       body: Center(
@@ -25,40 +27,6 @@ class _ProfilePageState extends State<ProfilePage> {
                 backgroundImage: NetworkImage(
                     'http://192.168.0.11:8000/api/avatar/${LoggedinUser.id}')),
             SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  username = value;
-                });
-              },
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Logika untuk menyimpan data
-                // Tambahkan kode di sini
-                print('Username: $username');
-                print('Email: $email');
-              },
-              child: Text('Save'),
-            ),
-            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 // Logika untuk mengedit foto
@@ -66,7 +34,80 @@ class _ProfilePageState extends State<ProfilePage> {
                 print('Edit Photo');
               },
               child: Text('Edit Photo'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFFFAC42),
+              ),
             ),
+            SizedBox(height: 20),
+            Container(
+              width: 350,
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  labelStyle: TextStyle(
+                      color:
+                          Color(0xFFFFFDFD)), // Set label text color to white
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFFFDFD)), // Set border color to white
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(
+                            0xFFFFFDFD)), // Set focused border color to white
+                  ),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    username = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 350,
+              child: TextField(
+                style: TextStyle(color: Color(0xFFFFFDFD)),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                      color:
+                          Color(0xFFFFFDFD)), // Set label text color to white
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFFFDFD)), // Set border color to white
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(
+                            0xFFFFFDFD)), // Set focused border color to white
+                  ),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 275,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Logika untuk menyimpan data
+                  // Tambahkan kode di sini
+                  print('Username: $username');
+                  print('Email: $email');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFFAC42),
+                ),
+                child: Text('Save'),
+              ),
+            ),
+            SizedBox(height: 10),
           ],
         ),
       ),
